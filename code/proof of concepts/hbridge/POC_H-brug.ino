1dc940a68acec49513385ab8df8d14853dd4b064
BryanCl7 <146442945+BryanCl7@users.noreply.github.com>
2023-11-24 15:15:09 +0100

    Add files via upload

---
+++ code/proof of concepts/hbridge/POC_H-brug.ino
@@ -0,0 +1,47 @@
+
+void setup() {
+  pinMode (5, OUTPUT); //rechts achteruit
+  pinMode (6, OUTPUT); //rechts vooruit
+  pinMode (13, OUTPUT); //links achteruit
+  pinMode (3, OUTPUT); //links vooruit
+
+}
+
+void loop() {
+ 
+//rechts achteruit
+    digitalWrite(5,HIGH);
+    digitalWrite(6, LOW); 
+
+    delay(1000);
+  
+//links achteruit 
+    digitalWrite(13,HIGH);
+    digitalWrite(3, LOW);
+
+delay(2000);
+
+//rechts vooruit
+    digitalWrite(6,HIGH); 
+    digitalWrite(5, LOW);
+  
+//links vooruit
+    digitalWrite(3,HIGH);
+    digitalWrite(13, LOW);
+
+delay(2000);
+
+
+    digitalWrite(6,LOW); 
+    digitalWrite(5, LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(13
+    , LOW);
+ 
+   for (int i = 50; i <= 255; i++){
+    analogWrite(6,i);
+   analogWrite(3,i);
+    delay(50);
+   }
+}
+    
